bd6f3e707848407b716ec706fe3b640c6ef63897
achihuahua22 <achihuahua22@gmail.com>
2024-11-20 15:17:13 -0700

    Serial print loop

--- pi_communication.py
+++ pi_communication.py
@@ -1,4 +1,9 @@
 import serial
-
+import roslibpy
 # Initialize Serial Port
 ser = serial.Serial(port = 'dev/tty/AMA0', baud = 9600, parity= serial.PARITY_NONE,timeout=1)
+
+# Repeatedly print serial line.
+while 1:
+    x = ser.readline()
+    print(x)
