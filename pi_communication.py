4d072bfee051b2492f56d523b7234913d9773b96
Alex Harris <128424786+achihuahua22@users.noreply.github.com>
2024-11-23 22:50:13 -0700

    imported spi and roslibpy packages

--- pi_communication.py
+++ pi_communication.py
@@ -1,9 +1,18 @@
 import serial
 import roslibpy
-# Initialize Serial Port
-ser = serial.Serial(port = 'dev/tty/AMA0', baud = 9600, parity= serial.PARITY_NONE,timeout=1)
+from smbus2 import SMBus
 
-# Repeatedly print serial line.
+# create a smbus object
+bus = SMBus(1)
+b = bus.read_byte_data(80,0)
+
+#look at spi adapter documentation to see address information
+#might need to move one of the dip switches.
+
+# Repeatedly print spi byte data.
 while 1:
-    x = ser.readline()
-    print(x)
+    print(b)
+
+
+#need to turn the two inputs in the spi bus into two distinct publisher nodes, then done i think :)
+client = roslibpy.Ros(host='localhost', port=9090)
