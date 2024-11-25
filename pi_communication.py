47236cffb326e04ecdb47f346e3a18f87b06fe91
Alex Harris <128424786+achihuahua22@users.noreply.github.com>
2024-11-24 20:21:32 -0700

    rostest

--- pi_communication.py
+++ pi_communication.py
@@ -28,14 +28,16 @@ from std_msgs.msg import String
 
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+    rospy.init_node("test_node")
+    rospy.loginfo("hello")
+    # pub = rospy.Publisher('chatter', String, queue_size=10)
+    # rospy.init_node('talker', anonymous=True)
+    # rate = rospy.Rate(10) # 10hz
+    # while not rospy.is_shutdown():
+    #     hello_str = "hello world %s" % rospy.get_time()
+    #     rospy.loginfo(hello_str)
+    #     pub.publish(hello_str)
+    #     rate.sleep()
 
 if __name__ == '__main__':
     try:
